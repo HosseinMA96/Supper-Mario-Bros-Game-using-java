e0cc2ac84577bec33407c16d1acfc5212c4ebe47
Hossein <hma.globe@gmail.com>
2020-01-21 16:01:19 +0330

    Entity and Tile Classes were modified. render and tick method were added

---
+++ src/Mario/tile/Tile.java
@@ -0,0 +1,85 @@
+package Mario.tile;
+
+import java.awt.*;
+
+public class Tile {
+    private int x,y,width,height,velX,velY;
+    private boolean solid;
+
+
+    public Tile(int x, int y, int width, int height,boolean solid) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.solid=solid;
+    }
+
+    /**
+     * Instead of creating bufferStrategies we use grapgics so
+     * the game will not lag due to many objects displayed in the screen
+     * @param g
+     */
+    public void render(Graphics g)
+    {
+
+    }
+
+    /**
+     * Update this entity
+     */
+    public void tick()
+    {
+        x+=velX;
+        y+=velY;
+    }
+
+    public boolean getSolid(){
+        return solid;
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setVelX(int velX) {
+        this.velX = velX;
+    }
+
+    public void setVelY(int velY) {
+        this.velY = velY;
+    }
+
+    public void setSolid(boolean solid) {
+        this.solid = solid;
+    }
+}
